18c468bf406d037eada1b3b649700ddacdbd8974
SirLeon <88978827@qq.com>
2018-07-31 17:06:58 +0800

    Initial commit

---
+++ src/main/java/com/formula/RegisterApp.java
@@ -0,0 +1,15 @@
+package com.formula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegisterApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(RegisterApp.class, args);
+    }
+}
