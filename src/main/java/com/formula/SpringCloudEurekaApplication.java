302f4ded84b664bc0614ef8e3498d91216c97e95
SirLeon <88978827@qq.com>
2018-08-01 17:25:30 +0800

    Single registry center.

--- src/main/java/com/formula/SpringCloudEurekaApplication.java
+++ src/main/java/com/formula/SpringCloudEurekaApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class RegisterApp
+public class SpringCloudEurekaApplication
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(RegisterApp.class, args);
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
     }
 }
